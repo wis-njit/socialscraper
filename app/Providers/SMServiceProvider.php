38694a00ef5765726fd05ea6c7825c914001db38
ethesx <ethesx@gmail.com>
2017-03-21 21:51:47 -0400

    REmoved unused imports

--- app/Providers/SMServiceProvider.php
+++ app/Providers/SMServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Providers\SMServiceProviderInterface;
-use app\Services\SMService;
 use Illuminate\Support\ServiceProvider;
 
 class SMServiceProvider extends ServiceProvider
