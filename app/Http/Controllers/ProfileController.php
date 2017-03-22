ce7b81eeb2fed56efe3b106acb0f13522cbb4c88
ethesx <ethesx@gmail.com>
2017-03-21 22:17:31 -0400

    Supply users' active providers to view

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,15 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Provider;
 use Illuminate\Http\Request;
+use App\Services\UserService;
 
 class ProfileController extends Controller
 {
-    public function __construct(){
+
+    private $userService;
+
+    public function __construct(UserService $userService){
         $this->middleware('auth');
+        $this->userService = $userService;
     }
     public function profile()
     {
         return view('profile');
+    }
+
+    public function getActiveProviders(){
+        $accounts = $this->userService->getAllProviderAccounts();
+        $providers = Provider::getAllProviderNames();
+
+        return view('profile', compact('accounts', 'providers'));
     }
 }
