b52c09c7348bf7b104d2abbb7c53efcc3119b98b
ethesx <ethesx@gmail.com>
2017-03-23 22:53:52 -0400

    Added UserService function to return all users' active SNS accounts Added unit test

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -1,13 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ethesx
- * Date: 3/21/2017
- * Time: 9:48 PM
- */
 
 namespace app\Services;
 
+use DB;
 
 class UserService
 {
@@ -19,8 +14,19 @@ class UserService
     /**
      *Retrieve all the providers' name for which the user has an account with
      */
-    public function getAllProviderAccounts(){
-        //TODO method stub
+    public function getAllProviderAccounts($userId){
+          return \DB::table('providers as p')
+            ->select('name',
+                    DB::raw('(CASE WHEN exists(
+                                SELECT provider_type_id
+                                FROM provider_user_profiles pu
+                                WHERE p.id = pu.provider_type_id 
+                                AND pu.user_id = ?) 
+                             THEN 1 ELSE 0 END) as active'
+                )
+            )->addBinding($userId, 'select')
+            ->get();
+
     }
-    
+
 }
---
+++ tests/Unit/UserServiceTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Services\UserService;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class UserServiceTest extends TestCase
+{
+
+    use DatabaseMigrations;
+
+    private $userService;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->userService = new UserService();
+        $this->seed("TestDatabaseSeeder");
+        //\DB::connection()->enableQueryLog();
+    }
+
+    public function testGetAllProviderAccounts(){
+
+        //Not in DB seeding
+        $out = $this->userService->getAllProviderAccounts('-1');
+        //print_r($out[0]->name);
+        self::assertTrue($out[0]->name == 'google');
+        self::assertTrue($out[0]->active == '0');
+
+        //Contained in DB seeding
+        $out = $this->userService->getAllProviderAccounts('1');
+        //print_r($out[0]->name);
+        self::assertTrue($out[0]->name == 'google');
+        self::assertTrue($out[0]->active == '1');
+        //print_r($out);
+        /*print_r(
+            \DB::getQueryLog()
+        );*/
+    }
+}
