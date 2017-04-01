124e75eaf77d4727934e0f4cf728a734e858147c
ethesx <ethesx@gmail.com>
2017-03-31 22:42:40 -0400

    Added utility

--- config/constants/SocialProvidersEnum.php
+++ config/constants/SocialProvidersEnum.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace config\constants;
-
+use ReflectionClass;
 
 abstract class SocialProvidersEnum
 {
@@ -9,4 +9,24 @@ abstract class SocialProvidersEnum
     const FACEBOOK = 'facebook';
     const TWITTER = 'twitter';
     const INSTAGRAM = 'instagram';
+
+    public static function isValidValue($value){
+        $r = new ReflectionClass(self::class);
+        $constants = $r->getConstants();
+        foreach ($constants as $constant){
+            if($constant === $value)
+                return true;
+        }
+        return false;
+    }
+
+    public static function getByValue($inputVal){
+        $r = new ReflectionClass(self::class);
+        $constants = $r->getConstants();
+        foreach ( $constants as $name => $value ){
+            if ( $value === $inputVal )
+                return $name;
+        }
+        return null;
+    }
 }
