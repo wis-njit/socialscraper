d2b754153b85d704301d530d2b877cdb3906fea2
ethesx <ethesx@gmail.com>
2017-05-13 16:20:26 -0400

    Comments

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,10 +3,16 @@
 namespace app\Services;
 
 use App\ProviderUserProfile;
-use App\User;
 use DB;
 use Laravel\Socialite\AbstractUser;
+use app\User;
 
+/**This service is utilized throughout the application and facilitates functions relevant
+ * to current users and their data
+ *
+ * Class UserService
+ * @package app\Services
+ */
 class UserService
 {
     public function __construct()
@@ -14,10 +20,12 @@ class UserService
     }
 
 
-    /**
-     *Retrieve all the providers' name for which the user has an account with
+    /**Retrieve all the providers' name for which the user has an account with
+     *
+     * @param string $userId
+     * @return \Illuminate\Support\Collection
      */
-    public function getAllProviderAccounts($userId){
+    public function getAllProviderAccounts(string $userId){
           return \DB::table('providers as p')
             ->select('name',
                     DB::raw('(CASE WHEN exists(
@@ -34,7 +42,14 @@ class UserService
     }
 
     //TODO need future ability to disassociate multiple provider accounts
-    public function disassociateProviderAccount($oauthProvider, $userId){
+
+    /**Disassociate(unlink) the provider from the user's account via a soft delete
+     *
+     * @param string $oauthProvider
+     * @param string $userId
+     * @return bool|mixed|null
+     */
+    public function disassociateProviderAccount(string $oauthProvider, string $userId){
         return ProviderUserProfile::whereHas('providerName' , function($query) use ($oauthProvider){
             $query->where('name', $oauthProvider);
         })
@@ -42,15 +57,27 @@ class UserService
             ->delete();
     }
 
-    public function getUserProviderProfile($id, $oauthProvider){
+    /**Returns the ProviderUserProfile given a user id
+     *
+     * @param string $userId
+     * @param string $oauthProvider
+     * @return ProviderUserProfile|mixed|null
+     */
+    public function getUserProviderProfile(string $userId, string $oauthProvider){
         return ProviderUserProfile::whereHas('providerName' , function($query) use ($oauthProvider){
             $query->where('name', $oauthProvider);
         })
-            ->where('user_id', $id)
+            ->where('user_id', $userId)
             ->first();
     }
 
-    public function getUserProviderProfileByProviderId($pid, $oauthProvider){
+    /**Returns the ProviderUserProfile given a provider's user id
+     *
+     * @param string $pid
+     * @param string $oauthProvider
+     * @return ProviderUserProfile|mixed|null
+     */
+    public function getUserProviderProfileByProviderId(string $pid, string $oauthProvider){
         return ProviderUserProfile::whereHas('providerName' , function($query) use ($oauthProvider){
             $query->where('name', $oauthProvider);
         })
@@ -59,26 +86,31 @@ class UserService
     }
 
     //TODO add helper function for current user
+    /**Update the OAuth access token for the user's provider account
+     *
+     * @param AbstractUser $user
+     * @param string $oauthProvider
+     */
     public function updateUserSNSToken(AbstractUser $user, User $authUser, string $oauthProvider){
 
         $profile = $this->getUserProviderProfileByProviderId($user->getId(), $oauthProvider);
         $profile->access_token = $user->token;
         $profile->access_token_key = isset($user->tokenSecret) ? $user->tokenSecret : '';
-        $this->updateProviderUserProfile($profile);
-    }
-
-    public function updateUser(User $user){
-        $user->save();
-    }
-
-    public function updateProviderUserProfile(ProviderUserProfile $profile){
         $profile->save();
     }
 
+    /**Add the provider the user logged in with to the session
+     *
+     * @param string $oauthProvider
+     */
     public function setCurrentSNSProvider(string $oauthProvider){
         session(['currentProvider' => $oauthProvider]);
     }
 
+    /**Retrieve the provider that the user is currently logged in with
+     *
+     * @return mixed
+     */
     public function getCurrentSNSProvider(){
         return session('currentProvider');
     }
