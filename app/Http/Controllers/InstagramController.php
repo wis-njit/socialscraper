6853d811337bd17aa23bf7f9b785a0c82f7d381a
ethesx <ethesx@gmail.com>
2017-05-13 22:32:19 -0400

    Refactored controller

--- app/Http/Controllers/InstagramController.php
+++ app/Http/Controllers/InstagramController.php
@@ -9,48 +9,15 @@ use Auth;
 use Config\Constants\SocialProvidersEnum;
 use GuzzleHttp\Psr7\Request;
 
-class InstagramController extends Controller
+class InstagramController extends ApiController
 {
-    //Mock data to return to view in the absence of API response
-    const mockDataHead = '{"';
-    const mockData = ' ":[
-                          {
-                             "id":1,
-                             "first_name":"Raymond",
-                             "last_name":"Ramos",
-                             "email":"rramos0@alexa.com",
-                             "gender":"Male",
-                             "ip_address":"165.64.230.96"
-                          },
-                          {
-                             "id":2,
-                             "first_name":"Mildred",
-                             "last_name":"Wood",
-                             "email":"mwood1@joomla.org",
-                             "gender":"Female",
-                             "ip_address":"110.252.219.18"
-                          },
-                          {
-                             "id":3,
-                             "first_name":"Frances",
-                             "last_name":"Greene",
-                             "email":"fgreene2@free.fr",
-                             "gender":"Female",
-                             "ip_address":"154.145.59.188"
-                          }
-                       ]
-                    }';
+
     //The API's base URI on which all requests will be made
     const URI = 'https://api.instagram.com';
 
-    protected $smServiceProvider;
-    protected $userService;
-
-    public function __construct(SMService $smsProvider, UserService $userService)
+    public function __construct(SMService $smProvider, UserService $userService)
     {
-        $this->middleware('auth');
-        $this->smServiceProvider = $smsProvider;
-        $this->userService = $userService;
+        parent::__construct( $smProvider,  $userService, SocialProvidersEnum::INSTAGRAM);
     }
 
     /**
@@ -72,12 +39,10 @@ class InstagramController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index($instUserId = null, $accessToken = null){
-        $snsProfile = $this->userService->getUserProviderProfile(Auth::id(), SocialProvidersEnum::INSTAGRAM);
+        $instUserId = $this->getProviderUserId();
+        $accessToken = $this->getProviderToken();
         $accounts = $this->userService->getAllProviderAccounts(Auth::id());
         $client = new Client(['base_uri' => self::URI]);
-
-        $instUserId = $snsProfile->provider_user_id;
-        $accessToken = $snsProfile->access_token;
 
         //TODO refactor using URI template
         /**Use Guzzle to make all requests in parallel and write responses
